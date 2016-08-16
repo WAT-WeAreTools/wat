cd5b93284e622bde668c80c15afef1d5d6fa7552
Andy Knolla <andyknolla@gmail.com>
2016-08-16 10:51:02 -0600

    add bootstrap layout, make header with logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,23 +10,67 @@
 //   }
 // }
 
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+
 class Main extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <AppBar title="W.A.T." />
+      <Card>
+  <CardHeader
+    title="URL Avatar"
+    subtitle="Subtitle"
+    avatar="images/jsa-128.jpg"
+  />
+  <CardMedia
+    overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+  >
+    <img src="images/nature-600-337.jpg" />
+  </CardMedia>
+  <CardTitle title="Card title" subtitle="Card subtitle" />
+  <CardText>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+  </CardText>
+  <CardActions>
+    <FlatButton label="Action1" />
+    <FlatButton label="Action2" />
+  </CardActions>
+</Card>
       </MuiThemeProvider>
     );
   }
 }
 
 export default Main;
+
+
+
+
+
+
+
+
 //
 // import React, {Component} from 'react';
 // import RaisedButton from 'material-ui/RaisedButton';
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
+import Main from './components/Main'; // Our custom react component
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
